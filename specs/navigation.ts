b1e092409bde07617636c1bff0390be9f66e273d
Sergey Sosnitskiy <sergey.sosnitskiy@thomascookonline.com>
2018-02-05 18:57:22 +0200

    Tests

---
+++ specs/navigation.ts
@@ -0,0 +1,16 @@
+import { browser, $, $$, element, by} from 'protractor'
+
+describe('Navigation ',async function() {
+
+    xit('should open "Upcoming movies" section', async function() {
+
+    })
+
+    xit('should open "Popular Series" section'), async function(){
+
+    }
+
+    xit('should open "Action" category', async function(){
+
+    })
+})
---
+++ specs/search.ts
@@ -0,0 +1,16 @@
+import { browser, $, $$, element, by} from 'protractor'
+
+describe('Search ', async function(){
+
+    xit('by exisiting name, should show first movie with complete name match', async function(){
+
+    })
+
+    xit('results(all of them) should contain serach request'), async function(){
+
+    }
+
+    xit('result should be empty, after request for nonexistent movie'), async function(){
+
+    }
+})
