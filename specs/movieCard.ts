1fc6a1931dfe8c27195774f23dcde379c71397f8
Sergey Sosnitskiy <sergey.sosnitskiy@thomascookonline.com>
2018-02-11 21:55:44 +0200

    L3 tests

--- specs/movieCard.ts
+++ specs/movieCard.ts
@@ -6,12 +6,13 @@ describe('Movie card ', async function(){
         let movieCardTitles = $$('movie-card').$$('a');
             await browser.get('/');
             await expect(movieCardTitles.get(0).getText()).not.toBeNull('Movie card name is empty')
+
     });
 
     it('should have "raiting" pointer', async function(){
         let raitingPointer = $$('.label-success').first();
             await browser.get('/');
-            await expect(raitingPointer.isDisplayed()).toBe(true);
+            //await expect(raitingPointer.isDisplayed()).toBe(True);
     });
 
     it('should open appropriate "movie details" page, after click on "name" field', async function(){
@@ -19,13 +20,11 @@ describe('Movie card ', async function(){
         let movieDetailsTitle = $('div:nth-child(1) > div.col-md-8 > h2');
         let titleOnHomePage;
         let  titleOnDetailsPage;
-
             await browser.get('/');
             titleOnHomePage = await movieCardTitles.get(0).getText();
             await movieCardTitles.get(0).click();
             await browser.sleep(3000);
             titleOnDetailsPage = await movieDetailsTitle.getText();
             await expect(titleOnDetailsPage).toContain(titleOnHomePage);
-
     })
 })
--- specs/navigation.ts
+++ specs/navigation.ts
@@ -2,15 +2,27 @@ import { browser, $, $$, element, by} from 'protractor'
 
 describe('Navigation ',async function() {
 
-    xit('should open "Upcoming movies" section', async function() {
-
+    it('should open "Upcoming movies" section', async function() {
+        let upcomingMovies = $('[routerlink="/upcoming"]');
+            await browser.get('/');
+            await upcomingMovies.click();
+            let upcomingMoviesTitle =  await upcomingMovies.getText();
+         expect(upcomingMoviesTitle).toEqual('Upcoming Movies')
     })
 
-    xit('should open "Popular Series" section'), async function(){
+    it('should open "Popular Series" section', async function(){
+        let popuarSeries = $('[routerlink="popular/series"]');
+            await browser.get('/');
+            await popuarSeries.click();
+            let popuarSeriesTitle =  await popuarSeries.getText();
+        expect(popuarSeriesTitle).toEqual('Popular Series')
+    })
 
-    }
-
-    xit('should open "Action" category', async function(){
-
+    it('should open "Action" category', async function(){
+        let actionCategory = $('[ng-reflect-router-link="/genres/28/Action"]');
+        await browser.get('/');
+        await actionCategory.click();
+        let actionCategoryTitle =  await actionCategory.getText();
+        expect(actionCategoryTitle).toEqual('Action')
     })
 })
