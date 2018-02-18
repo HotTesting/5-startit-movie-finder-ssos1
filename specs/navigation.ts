94b913900d6b544caa6ac2b2fa2a1a85daaa2e35
Sergey Sosnitskiy <sergey.sosnitskiy@thomascookonline.com>
2018-02-18 18:54:00 +0200

    fix_for_test

--- specs/navigation.ts
+++ specs/navigation.ts
@@ -4,17 +4,19 @@ describe('Navigation ',async function() {
 
     it('should open "Upcoming movies" section', async function() {
         let upcomingMovies = $('[routerlink="/upcoming"]');
-            await browser.get('/');
-            await upcomingMovies.click();
-            let upcomingMoviesTitle =  await upcomingMovies.getText();
-         expect(upcomingMoviesTitle).toEqual('Upcoming Movies')
+        await browser.get('/');
+        await upcomingMovies.click();
+        expect(await browser.getCurrentUrl()).toEqual(await browser.baseUrl + "upcoming")
+        let upcomingMoviesTitle =  await upcomingMovies.getText();
+        expect(upcomingMoviesTitle).toEqual('Upcoming Movies')
     })
 
     it('should open "Popular Series" section', async function(){
         let popuarSeries = $('[routerlink="popular/series"]');
-            await browser.get('/');
-            await popuarSeries.click();
-            let popuarSeriesTitle =  await popuarSeries.getText();
+        await browser.get('/');
+        await popuarSeries.click();
+        expect(await browser.getCurrentUrl()).toEqual(await browser.baseUrl + "popular/series")
+        let popuarSeriesTitle =  await popuarSeries.getText();
         expect(popuarSeriesTitle).toEqual('Popular Series')
     })
 
@@ -22,6 +24,7 @@ describe('Navigation ',async function() {
         let actionCategory = $('[ng-reflect-router-link="/genres/28/Action"]');
         await browser.get('/');
         await actionCategory.click();
+        expect(await browser.getCurrentUrl()).toEqual(await browser.baseUrl + "genres/28/Action")
         let actionCategoryTitle =  await actionCategory.getText();
         expect(actionCategoryTitle).toEqual('Action')
     })
--- specs/search.ts
+++ specs/search.ts
@@ -3,44 +3,47 @@ import { browser, $, $$, element, by} from 'protractor'
 describe('Search ', async function(){
 
     it('by exisiting name, should show first movie with complete name match', async function(){
-        let movieCardTitles = $$('movie-card').$$('a');
+        let movieCardTitles = $$('movie-card a');
         let searchField = $('[name="searchStr"]');
         let goButton = $('.input-group-btn button');
         let searchResultGroup = $('movies > div:nth-child(3) > div').$$('movie-card').$$('a');
-            await browser.get('/');
-            let firstMovieName = await movieCardTitles.get(0).getText();
-            await browser.sleep(1000);
-            await searchField.sendKeys(firstMovieName);
-            await goButton.click();
-            await browser.sleep(1000);
-            let firstSearchedName = await searchResultGroup.get(0).getText();
-            expect(firstSearchedName).toEqual(firstMovieName);
+        await browser.get('/');
+        let firstMovieName = await movieCardTitles.get(0).getText();
+        await browser.sleep(1000);
+        await searchField.sendKeys(firstMovieName);
+        await goButton.click();
+        await browser.sleep(1000);
+        let firstSearchedName = await searchResultGroup.get(0).getText();
+        expect(firstSearchedName).toEqual(firstMovieName);
     });
 
     it('results (all of them) should contain search request', async function(){
-        let movieCardTitles = $$('movie-card').$$('a');
+        let searchQuery = 'action';
+        let movieCardTitles = $$('movie-card a');
         let searchField = $('[name="searchStr"]');
         let goButton = $('.input-group-btn button');
-        let searchResultGroup = $('movies > div:nth-child(3) > div').$$('movie-card').$$('h4 > a');
-            await browser.get('/');
-        let firstMovieName = await movieCardTitles.get(0).getText();
-            await browser.sleep(1000);
-            await searchField.sendKeys(firstMovieName);
-            await goButton.click();
-            await browser.sleep(1000);
+        await browser.get('/');
+        await browser.sleep(1000);
+        await searchField.sendKeys(searchQuery);
+        await goButton.click();
+        await browser.sleep(1000);
+        let searchResultGroup = $('movies > div:nth-child(3) > div').$$('movie-card').$$('h4 > a')
+            .asElementFinders_();
         let searchResults : any = await searchResultGroup;
-        await searchResults.forEach(searchResults => expect(searchResults.getText()).toContain(firstMovieName))
+        for (let searchElem of searchResults) {
+            await expect((await searchElem.getText()).toLowerCase()).toContain(searchQuery)
+        }
     });
 
     it('result should be empty, after request for nonexistent movie', async function(){
         let searchField = $('[name="searchStr"]');
         let goButton = $('.input-group-btn button');
         let searchResultGroup = $$('movies > div:nth-child(3) > div').$$('movie-card').$$('a');
-            await browser.get('/');
-            await browser.sleep(1000);
-            await searchField.sendKeys('shouldBeEmpty');
-            await goButton.click();
-            await browser.sleep(1000);
+        await browser.get('/');
+        await browser.sleep(1000);
+        await searchField.sendKeys('shouldBeEmpty');
+        await goButton.click();
+        await browser.sleep(1000);
         let searchResults = await searchResultGroup.count();
         expect(searchResults).toEqual(0);
     })
