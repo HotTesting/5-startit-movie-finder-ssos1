05763cdea6aed58f6cc58d6c5343925597f82f04
Sergey Sosnitskiy <sergey.sosnitskiy@thomascookonline.com>
2018-02-28 23:23:24 +0200

    Disabled reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,7 @@
 require('ts-node').register();
 
+
+
 module.exports.config = {
     specs: ['specs/*.ts'],
     baseUrl: 'https://movies-finder.firebaseapp.com/',
@@ -9,7 +11,18 @@ module.exports.config = {
         enableVNC: true,
         name: "SergeySS"
     },
-    onPrepare: async function() {
-        await browser.manage().timeouts().implicitlyWait(1000);
-    }
+    // onPrepare: async function () {
+    //     await
+    //     browser.manage().timeouts().implicitlyWait(1000);
+    // },
+
+    // Adding nice console output.
+//     // Provided by: https://github.com/razvanz/jasmine2-reporter
+//     let ConsoleReporter = require('jasmine2-reporter').Jasmine2Reporter
+//     let console_reporter_options = {
+//         startingSpec: true
+//     }
+//     jasmine.getEnv().addReporter(new ConsoleReporter(console_reporter_options))
+// }
 }
+
